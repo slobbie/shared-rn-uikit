8616ccf9861e57e25b45f1aa1a066c2c9d39079d
haeseok <haeseok@minirecord.com>
2025-05-22 09:36:02 +0900

    [Refactor] 기본 input 컴포넌트 스타일 수정

--- shared/ui/atoms/textInput/TextInputBase.tsx
+++ shared/ui/atoms/textInput/TextInputBase.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, TextInput } from 'react-native';
+import { TextInput, TextInputProps } from 'react-native';
 
 import { IBaseInputProps } from '@shared/ui/atoms/textInput/textInput.interface';
+import { useTheme } from '@shared/utils/lib/ThemeContext';
 
 /**
  * 기본 TextInput 컴포넌트
@@ -11,6 +12,30 @@ const TextInputBase = ({
   placeholderColor = '#AAAEB6',
   ...props
 }: IBaseInputProps) => {
+  const { theme } = useTheme();
+
+  const styles = {
+    base: {
+      width: '100%',
+      height: 56,
+      backgroundColor: theme.colors.background.secondary,
+      paddingHorizontal: 16,
+      paddingVertical: 12,
+    } as TextInputProps,
+    default: {
+      borderRadius: 14,
+    },
+    outlined: {
+      borderRadius: 14,
+      borderWidth: 1,
+      borderColor: theme.colors.border.secondary,
+    },
+    underlined: {
+      borderBottomWidth: 1,
+      borderBottomColor: theme.colors.border.secondary,
+    },
+  };
+
   return (
     <TextInput
       placeholderTextColor={placeholderColor}
@@ -21,25 +46,3 @@ const TextInputBase = ({
 };
 
 export default TextInputBase;
-
-const styles = StyleSheet.create({
-  base: {
-    width: '100%',
-    height: 56,
-    backgroundColor: '#F6F7F9',
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-  },
-  default: {
-    borderRadius: 14,
-  },
-  outlined: {
-    borderRadius: 14,
-    borderWidth: 1,
-    borderColor: '#222',
-  },
-  underlined: {
-    borderBottomWidth: 1,
-    borderBottomColor: '#222',
-  },
-});

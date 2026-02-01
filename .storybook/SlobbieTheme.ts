0feee9ea30b40bc32c52d4579c1f306e57026e49
junghaeseok <haeru9410@gmail.com>
2026-02-01 23:50:44 +0900

    [docs] Configure.mdx 파일 수정

---
+++ .storybook/SlobbieTheme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Slobbie',
+  brandUrl: 'https://github.com/slobbie/shared-rn-uikit',
+  brandImage: '../stories/assets/logo.png',
+  brandTarget: '_self',
+});
